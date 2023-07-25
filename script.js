4bd17dc7650f95bc17ecd8e520d71b32e449e17b
Keerthish17 <114150536+Keerthish17@users.noreply.github.com>
2023-07-25 11:38:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("b2").addEventListener('click', bf2);
+function bf2(){
+document.getElementById("car").src="mus2.jpg"
+}
+document.getElementById("b3").addEventListener('click', bf3);
+function bf3(){
+document.getElementById("car").src="mus3.png"
+}
+document.getElementById("b1").addEventListener('click', bf1);
+function bf1(){
+document.getElementById("car").src="car1.jpg"
+}
+document.getElementById("cart").addEventListener('click', car);
+function car(){
+document.getElementById("cart").innerHTML="Item added to cart";
+}
